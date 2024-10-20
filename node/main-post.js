36cc17e4ee0014fa6ac23d47f9530ca8953e863c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-20 08:01:52 -0400

    ...

--- node/main-post.js
+++ node/main-post.js
@@ -1,7 +1,7 @@
 function getInput(name, options) {
   const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
   if (options && options.required && !val) {
-    console.log(Object.keys(process.env).sort().join(', '));
+    console.log('post error:' + Object.keys(process.env).sort().join(', '));
     throw new Error(`Input required and not supplied: ${name}`);
   }
   return val.trim();
--- node/main.js
+++ node/main.js
@@ -1,7 +1,7 @@
 function getInput(name, options) {
   const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  console.log('main: '+Object.keys(process.env).sort());
   if (options && options.required && !val) {
-    console.log(Object.keys(process.env).sort());
     throw new Error(`Input required and not supplied: ${name}`);
   }
   return val.trim();
