8a0b13aeb52f7745ea9ac808e497806f9b51f9d3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-20 07:57:40 -0400

    ...

--- node/main-post.js
+++ node/main-post.js
@@ -1,6 +1,7 @@
 function getInput(name, options) {
   const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
   if (options && options.required && !val) {
+    console.log(Object.keys(process.env).sort());
     throw new Error(`Input required and not supplied: ${name}`);
   }
   return val.trim();
