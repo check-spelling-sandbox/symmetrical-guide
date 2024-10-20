1516f0a2bdd0d9ac8f03ad1d93ee42819fa9c9ac
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-20 16:49:39 -0400

    workaround?

--- node/main-post.js
+++ node/main-post.js
@@ -1,5 +1,6 @@
 function getInput(name, options) {
-  const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  const key = name.replace(/ /g, '_').toUpperCase();
+  const val = process.env[`STATE_${key}`] || process.env[`INPUT_${key}`] || '';
   console.log('post: ' + JSON.stringify(process.env));
   if (options && options.required && !val) {
     throw new Error(`Input required and not supplied: ${name}`);
--- node/main.js
+++ node/main.js
@@ -1,10 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+
 function getInput(name, options) {
-  const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  const key = name.replace(/ /g, '_').toUpperCase();
+  const val = process.env[`INPUT_${key}`] || '';
   console.log('main: '+JSON.stringify(process.env));
   if (options && options.required && !val) {
     throw new Error(`Input required and not supplied: ${name}`);
   }
-  return val.trim();
+  const trimmedVal = val.trim();
+  fs.appendFileSync(process.env.GITHUB_STATE, `${key}=${trimmedVal}`, {
+    encoding: 'utf8'
+  });
+  return trimmedVal;
 }
 
 console.log('node (main) ' + getInput('test', {required: true}));
